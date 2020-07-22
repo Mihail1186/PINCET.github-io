509f4b58c8f95e6491c0cd689cb43ea85f66413e
Mihail Shestak <66316437+Mihail1186@users.noreply.github.com>
2020-07-22 21:21:54 +0300

    сделал бургер

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ $(document).ready(function () {
    })
 
 })
+
+
+$(document).ready(function () {
+   $('.nav__burger').click(function (event) {
+      $('.nav__burger, .menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   })
+
+})
+
